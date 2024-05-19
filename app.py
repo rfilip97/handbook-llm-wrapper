8cfcb9d42ef2000b1f0e39159869c70c197631b9
Filip Razvan <filip.razvan@agilefreaks.com>
2024-05-19 10:24:23 +0300

    Use prompt template

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from langchain_community.llms import Ollama
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
 
 llm = Ollama(model="llama3")
 
-response = llm.invoke("What is the meaning of life?")
+template = """
+Provide the number of legs the following animal has, or
+type 'NONE' if it doesn't apply. Please always respond only
+with either a number which is the response, or with NONE
+
+text: {input}
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template=template)
+chain = LLMChain(llm=llm, prompt=prompt_template)
+
+user_input = input("Please enter the text you want to process: ")
+response = chain.predict(input=user_input)
 
 print(response)
 
