a44573c19784086209c9b6b3a626b4fe3a761e3d
Filip Razvan <filip.razvan@agilefreaks.com>
2024-06-28 14:14:08 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="af_andbook_assistant",
+    name="handbook_llm_wrapper",
     version="0.1.0",
     description="An assistant package",
-    packages=find_packages(),
+    packages=find_packages(include=['handbook_llm_wrapper', 'handbook_llm_wrapper.*']),
     install_requires=[
-        "langchain_community"
-        "pickle-mixin"
-        "faiss-cpu"
-        "langchain"
-        "llama-cpp-python"
-        "unstructured[md]"
-        "sentence-transformers"
+        "langchain_community",
+        "pickle-mixin",
+        "faiss-cpu",
+        "langchain",
+        "llama-cpp-python",
+        "unstructured[md]",
+        "sentence-transformers",
     ],
     entry_points={
         "console_scripts": [
-            "assistant=assistant.__main__:main",
+            "assistant=handbook_llm_wrapper.main:main",
         ],
     },
     python_requires=">=3.12",
 )
+
